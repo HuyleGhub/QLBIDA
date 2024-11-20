147f95b5a3f39c2f2d3e17b4587318d0835cca14
lehuy <lehuytran48@gmail.com>
2024-11-20 16:53:23 +0700

    Add project files.

---
+++ QLBIDA.Module/BusinessObjects/QLBIDACode/HoaDon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+namespace QLBIDA.Module.BusinessObjects.QLBIDA
+{
+
+    public partial class HoaDon
+    {
+        public HoaDon(Session session) : base(session) { }
+        public override void AfterConstruction() { 
+            base.AfterConstruction();
+            if (Session.IsNewObject(this))
+            {
+                ngayTao = DateTime.Now;
+            }
+        }
+    }
+
+}
